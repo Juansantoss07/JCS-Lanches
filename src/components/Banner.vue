8ef511dd477817aafa4aeb4f68b8d4670bb300b0
Juansantoss07 <juansantosscunha@gmail.com>
2022-02-15 15:30:38 -0300

    fix:update

---
+++ src/components/Banner.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="main-banner">
+        <h1>JCS Lanches</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "Banner"
+    }
+</script>
+
+<style scoped>
+    #main-banner{
+        background-image: url('/img/burger.jpeg');
+        background-position: 0 -250px;
+        background-size: cover;
+        width: 100%;
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        }
+
+    #main-banner h1{
+        color: #fff;
+        background-color:#222;
+        text-align: center;
+        font-size: 60px;
+        padding: 20px 40px;
+    }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav>
+    <Router-link to="/" id="logo_url">
+    <img :src="logo" :alt="alt" id="logo"></Router-link>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/pedidos">Pedidos</RouterLink>
+  </nav>
+</template>
+
+<script>
+    export default{
+        name: "Navbar",
+        props:[
+            "logo", "alt"
+        ]
+    }
+</script>
+
+<style scoped>
+    nav{
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding: 15px 50px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+    
+    #logo{
+        width: 40px;
+        border-radius: 5px;
+    }
+
+    #logo_url{
+        margin: auto;
+        margin-left: 0;
+    }
+
+    nav a{
+        color: #FCBA03;
+        text-decoration: none;
+        margin: 12px;
+        transition: 0.5s;
+    }
+
+    nav a:hover{
+        color: #fff;
+    }
+</style>
